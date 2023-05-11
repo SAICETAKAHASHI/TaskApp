3e57d00d43547f5d1248c78f57261fed4f4f1ff5
SAICETAKAHASHI <takahashi@saice.co.jp>
2023-05-11 09:56:23 +0900

    renderデプロイテスト1回目

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module TaskApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Hosts を追加
+    config.hosts << 'sample-rails.onrender.com'
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
